dada2b69f3a9ac8bade6e95bb247463db114654d
sus <rushanyan8477@gmail.com>
2026-05-22 12:35:39 +0300

    refactor: add dedcision DB or CHAT

--- ui.py
+++ ui.py
@@ -7,7 +7,6 @@ st.title('R-407: Чат с дроидом')
 
 @st.cache_resource
 def load_agents():
-
     analyst = DatabaseAnalyst(db_path='./data/screen_time.db')
     main = MainAgent() 
     return analyst, main
@@ -21,15 +20,22 @@ for msg in st.session_state.messages:
     with st.chat_message(msg['role']):
         st.write(msg['content'])
 
-if prompt := st.chat_input('Спроси что-нибудь про свою статистику'):
-    st.session_state.messages.append({'role': 'user', 'content': prompt})
+if user_request := st.chat_input('Спроси что-нибудь про свою статистику'):
+    st.session_state.messages.append({'role': 'user', 'content': user_request})
     with st.chat_message('user'):
-        st.write(prompt)
+        st.write(user_request)
     
     with st.spinner('R-407 напрягает свои микросхемы...'):
         try:
-            raw_data = analyst_agent.get_data(prompt)
-            ai_answer = main_agent.answer(prompt, raw_data)
+            decision = main_agent.get_routing_decision(user_request)
+            if decision == 'DB':
+                st.toast('Активирован модуль анализа БД', icon='📊')
+                raw_data = analyst_agent.get_data(main_agent.translate_for_db_agent(user_request))
+                ai_answer = main_agent.answer(user_request, raw_data)
+            else:
+                st.toast('Активирован модуль диалога', icon='💬')
+                ai_answer = main_agent.answer(user_request, db_data="Данные БД не требуются. Веди обычный диалог в своем стиле.")
+        
         except Exception as e:
             ai_answer = f'Микросхемы воспылали... Ошибка: {e}'
 

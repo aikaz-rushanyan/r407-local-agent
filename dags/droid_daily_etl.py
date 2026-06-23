182475d29d0afe4fa9d3a779e883e1d60573a9cf
sus <rushanyan8477@gmail.com>
2026-06-23 14:31:43 +0300

    chore: update comments

--- dags/droid_daily_etl.py
+++ dags/droid_daily_etl.py
@@ -17,26 +17,6 @@ default_args = {
     'retry_delay': timedelta(minutes=2),
 }
 
-def send_tg_msg(txt):
-    if not TOKEN or not CHAT_ID:
-        print("Ошибка: Секреты Телеграма не найдены в переменных окружения!")
-        return
-    
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    payload = {
-        "chat_id": CHAT_ID,
-        "text": txt
-    }
-    
-    try:
-        response = requests.post(url, json=payload)
-        if response.status_code == 200:
-            print('Сообщение отправлено!')
-        else:
-            print(f'Что-то не так... {response.status_code}, {response.text}')
-    except Exception as e:
-        print(f'Ошибка: {e}')
-
 def send_report():
     db_path = '/opt/airflow/data/screen_time.db' 
     
@@ -64,13 +44,13 @@ def send_report():
 
 # Функция трансформации и очистки данных (наш SQL-запрос)
 def run_sql_aggregation():
-    # Путь внутри контейнера, который мы прокинули на Шаге 1
+    
     db_path = '/opt/airflow/data/screen_time.db' 
     
     with sqlite3.connect(db_path) as conn:
         cursor = conn.cursor()
         
-        # Сначала создаем таблицу витрины, если её еще нет
+        # создаем таблицу витрины, если её еще нет
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS daily_app_summary (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -86,7 +66,6 @@ def run_sql_aggregation():
         WHERE date(log_date) = date('now', 'localtime')
     ''')
         
-        #SQL-запрос агрегации
         print("Запуск агрегации сырых логов за сегодня...")
 
         cursor.execute('''
@@ -110,9 +89,9 @@ with DAG(
     default_args=default_args,
     schedule_interval='55 23 * * *',  # Запуск каждый день в 23:55
     start_date=datetime(2026, 6, 1), # Дата, с которой граф начинает гипотетически существовать
-    catchup=False                     # Не нужно лихорадочно выполнять задачи за прошлые дни при старте
+    catchup=False                     # Не нужно выполнять задачи за прошлые дни при старте
 ) as dag:
-    # Объявляем нашу задачу
+    
     aggregate_task = PythonOperator(
         task_id='aggregate_raw_logs',
         python_callable=run_sql_aggregation
@@ -127,10 +106,9 @@ with DAG(
     send_tg_task = TelegramOperator(
         task_id='send_telegram_report',
         telegram_conn_id='my_telegram_connection',
-        # Эта магия вытащит строку, которую вернул предыдущий таск
+        #вытащит строку, которую вернул предыдущий таск
         text="{{ ti.xcom_pull(task_ids='generate_report_text') }}",
-        chat_id='920398904'
+        chat_id=str(CHAT_ID)
     )
 
-    # Так как задача пока одна, цепочку из >> строить не нужно, она просто выполнится
     aggregate_task >> generate_report_task >> send_tg_task
